3835a4b6cfc7ac59a77d614f1435fb87715bd8b7
li_junfeng <li_junfeng@sicca.com.cn>
2025-11-18 16:46:46 +0800

    fix: 修复 vcpkg 工具链问题

--- cmake/module_vcpkg.cmake
+++ cmake/module_vcpkg.cmake
@@ -1,5 +1,5 @@
 # 设置 vcpkg 配置
-if(CMAKE_SYSTEM_NAME MATCHES "Windows")
+if(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
     # 获取 VCPKG_ROOT
     if(NOT DEFINED VCPKG_ROOT)
         if(DEFINED ENV{VCPKG_ROOT})
@@ -10,43 +10,37 @@ if(CMAKE_SYSTEM_NAME MATCHES "Windows")
     endif()
 
     # 设置工具链文件
-    set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
-
-    # 架构判断
-    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-        set(VCPKG_TARGET_TRIPLET "x64-windows-static")
-    else()
-        set(VCPKG_TARGET_TRIPLET "x86-windows-static")
-    endif()
+    set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
+    set(VCPKG_TARGET_TRIPLET "x64-windows-static")
+    set(PKG_CONFIG_EXECUTABLE "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/tools/pkgconf/pkgconf.exe")
+    set(PKG_CONFIG_PATH "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/lib/pkgconfig")
 
     # 路径输出
     message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
     message(STATUS "CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
-    message(STATUS "VCPKG_TARGET_TRIPLET: ${VCPKG_TARGET_TRIPLET}")
-
-    # 加载工具链
-    include(${CMAKE_TOOLCHAIN_FILE})
 endif()
 
-################################################################################
-# Eigen3
-#   vcpkg install eigen3:x64-windows-static
-################################################################################
-find_package(Eigen3 REQUIRED CONFIG)
-if(EIGEN3_FOUND)
-    include_directories(${EIGEN3_INCLUDE_DIRS})
-    include_directories(${EIGEN3_INCLUDE_DIRS}/../)
-else()
-    message(FATAL_ERROR "Eigen3 not found. Please install via: vcpkg install eigen3:${VCPKG_TARGET_TRIPLET}")
-endif()
+macro(VCPKG_LOAD_3RDPARTY)
+    ################################################################################
+    # Eigen3
+    #   vcpkg install eigen3:x64-windows-static
+    ################################################################################
+    find_package(Eigen3 CONFIG REQUIRED)
+    if(Eigen3_FOUND)
+        include_directories(${EIGEN3_INCLUDE_DIRS})
+        include_directories(${EIGEN3_INCLUDE_DIRS}/../)
+    else()
+        message(FATAL_ERROR "Eigen3 not found. Please install via: vcpkg install eigen3:${VCPKG_TARGET_TRIPLET}")
+    endif()
 
-################################################################################
-# OpenCV
-#   vcpkg install opencv:x64-windows-static
-################################################################################
-find_package(OpenCV CONFIG REQUIRED)
-if(OpenCV_FOUND)
-    include_directories(${OpenCV_INCLUDE_DIRS})
-else()
-    message(FATAL_ERROR "OpenCV not found. Please install via: vcpkg install opencv:${VCPKG_TARGET_TRIPLET}")
-endif()
+    ################################################################################
+    # OpenCV
+    #   vcpkg install opencv:x64-windows-static
+    ################################################################################
+    find_package(OpenCV CONFIG REQUIRED)
+    if(OpenCV_FOUND)
+        include_directories(${OpenCV_INCLUDE_DIRS})
+    else()
+        message(FATAL_ERROR "OpenCV not found. Please install via: vcpkg install opencv:${VCPKG_TARGET_TRIPLET}")
+    endif()
+endmacro()
